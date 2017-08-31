21e90dac76ac21cd473f7e8e0eafff3173cbc555
Josh Engelhardt <mrjoshte@gmail.com>
2017-08-31 13:11:31 -0500

    name change on the main - adding in bot auth

--- src/server/player-object.js
+++ src/server/player-object.js
@@ -4,4 +4,4 @@ class player {
 	var wins{fpSolo:0, fpDuos:0, fpSquad:0, tpSolo:0, tpDuos:0, tpSquad:0};
 	var kills{fpSolo:0, fpDuos:0, fpSquad:0, tpSolo:0, tpDuos:0, tpSquad:0};
 	var damage{fpSolo:0, fpDuos:0, fpSquad:0, tpSolo:0, tpDuos:0, tpSquad:0};
-	}
+}
