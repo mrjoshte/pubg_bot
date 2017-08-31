b58e2e2490b676bafae83c3554a9d139569fc003
Josh Engelhardt <mrjoshte@gmail.com>
2017-08-31 13:02:03 -0500

    re-arranging and adding in a player-object.js

---
+++ src/server/player-object.js
@@ -0,0 +1,7 @@
+class player {
+	var pubgName;
+	var discordName;
+	var wins{fpSolo:0, fpDuos:0, fpSquad:0, tpSolo:0, tpDuos:0, tpSquad:0};
+	var kills{fpSolo:0, fpDuos:0, fpSquad:0, tpSolo:0, tpDuos:0, tpSquad:0};
+	var damage{fpSolo:0, fpDuos:0, fpSquad:0, tpSolo:0, tpDuos:0, tpSquad:0};
+	}
