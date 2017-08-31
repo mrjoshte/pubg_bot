05f216245ea29af6d87910a41a657f3514d5da66
JoshE <mrjoshte@gmail.com>
2017-08-31 18:37:29 -0500

    New user logic has been implemented.

--- src/server/bot.js
+++ src/server/bot.js
@@ -2,19 +2,27 @@
  * Created by mrjoshte on 8/31/2017
  */
 //Link dependencies
-var auth = require('./auth.json');
-var server = require('server.js');
+var auth = require('../../auth.json');
+var server = require('./server.js');
+const Discord = require('discord.js');
 
 var bot = new Discord.Client();
 bot.login(auth.token);
 
 bot.on("ready", () => {
-  console.log("Hey everyone!");
+  console.log("Bot is started.");
 });
 
 bot.on("message", (message) => {
-	if (message.content.startsWith("!")) {
-		server.createNewPlayer(message.author, message.content);
+	console.log("Messaged detected");
+	if (message.content.startsWith("!addme ")) {
+		console.log("Adding new user");
+		if(server.createNewPlayer(message.author.id, message.content.substring(7, message.content.length))){
+			message.channel.send(message.content.substring(7, message.content.length) + " was added successfully!");
+		}
+		else{
+			message.channel.send(message.content.substring(7, message.content.length) + " could not be added.");
+		}
 	}
 });
 
--- src/server/server.js
+++ src/server/server.js
@@ -4,95 +4,103 @@
 //Link dependencies
 
 const {PubgAPI, PubgAPIErrors, REGION, SEASON, MATCH} = require('pubg-api-redis');
-const playerFile = "./storage/players.json";
-const Discord = require('discord.js');
+const playerFile = "../storage/players.json";
 const fs = require('fs');
 var pubgTrackerAPIKey = '21c941ec-f966-4919-ad40-7976405ca06b';
-const MATCH = {
-  SOLO: 'solo',
-  DUO: 'duo',
-  SQUAD: 'squad',
-};
 
 // If no Redis configuration it wont be cached
 const api = new PubgAPI({
   apikey: pubgTrackerAPIKey,
-  redisConfig: {
+  /*redisConfig: {
     host: '127.0.0.1',
     port: 6379,
     expiration: 300, // Optional - defaults to 300.
-  },
+  },*/
 });
 
 var getPlayerList = function(){
-	var listOfPlayers;
-	fs.readFile(playerFile, function(err, data){
-        if(err){
-            console.log(err);
-        } else{
-            listOfPlayers = JSON.parse(data);
-        }
-        if(startupCallback){ startupCallback() }
-    });
-}
-var fetchUpdatedPlayerData = function(savedPlayerList){
+	return JSON.parse(fs.readFileSync(playerFile));
+};
+
+var fetchUpdatedPlayerData = function(savedPlayerList, creatingNewPlayer){
 	//This will iterate through all the players
+	debugger;
 	for(var i=0; i<savedPlayerList.length; i++){
 			//This will fetch the player from the pubg api
-			api.getProfileByNickname(savedPlayerList[i].pubgName)
+			var player = savedPlayerList[i];
+			api.getProfileByNickname(player.pubgName)
 				  .then((profile) => {
 					const data = profile.content;
-					for(var j=0; j<MATCH.length; j++){
+					//for(var j=0; j<MATCH.length; j++){
+					Object.keys(MATCH).forEach(function(match) {
+						var matchType = MATCH[match];
 						stats = profile.getStats({
 						  region: REGION.NA,
-						  match: MATCH[j]
+						  match: matchType
 						});
 						//This is where we actually compare the saved vs pulled
-						if(stats.performance.wins > savedPlayerList[i][wins].MATCH[j]){
+						
+						if(stats.performance.wins > player.wins[matchType] && !creatingNewPlayer){
 							//save the new data to send to the server
 						var winner = new Object();
-							winner.kills = stats.combat.kills - savedPlayerList[i][kills].MATCH[j];
-							winner.damage = stats.support.damageDealt - savedPlayerList[i][damage].MATCH[j];
+							winner.kills = stats.combat.kills - player.kills[matchType];
+							winner.damage = stats.support.damageDealt - savedPlayerList[i].damage[matchType];
 							sendWinToDiscord(winner);
 						}
 						//update the file
-							savedPlayerList[i][wins].MATCH[j] = stats.performance.wins;
-							savedPlayerList[i][kills].MATCH[j] = stats.combat.kills;
-							savedPlayerList[i][damage].MATCH[j] = stats.support.damageDealt
+							player.wins[matchType] = stats.performance.wins;
+							player.kills[matchType] = stats.combat.kills;
+							player.damage[matchType] = stats.support.damageDealt
+					});
+					savedPlayerList = getPlayerList();
+					if(creatingNewPlayer){
+						savedPlayerList.push(player);
 					}
+					savedPlayerList.indexOf(player);
+					writeUpdatedPlayerListToFile(savedPlayerList);
 				  });
 		}
-	}
-	writeUpdatedPlayerListToFile(savedPlayerList);
-}
+};
 
 var writeUpdatedPlayerListToFile = function(playerList){
-	//TODO
-}
+	fs.writeFile(playerFile, JSON.stringify(playerList), function(err){
+		//something went wrong?
+	});
+};
 
 var sendWinToDiscord = function(winner){
 	//TODO
+};
+
+module.exports = 
+{	
+createNewPlayer: function(discordName, pubgName){
+	//double check that the name of this user isn't in the list already
+	var playerList = getPlayerList();
+	var newPlayer = true;
+	for(var i = 0; i < playerList.length; i++){
+		if(playerList[i].discordName === discordName){
+			newPlayer = false;
+		}
+	}
+	if(newPlayer){
+		debugger;
+		api.getProfileByNickname(pubgName)
+			.then((profile) => {
+				var player = new Object();
+				player.pubgName = pubgName;
+				player.discordName = discordName;
+				player.wins = {solo:0,duo:0,squad:0};
+				player.kills = {solo:0,duo:0,squad:0};
+				player.damage = {solo:0,duo:0,squad:0};
+				fetchUpdatedPlayerData([player], true);
+				return true;
+			}, function(error){
+				return false;
+			});
+	}
+	else{
+		return false;
+	}
 }
-
-var createNewPlayer = function(discordName, pubgName){
-	//TODO
-}
-
-api.getProfileByNickname(name)
-  .then((profile) => {
-    const data = profile.content;
-    const stats = profile.getStats({
-      region: REGION.NA, // defaults to profile.content.selectedRegion
-      season: SEASON.EA2017pre3, // defaults to profile.content.defaultSeason
-      match: MATCH.SOLO // defaults to SOLO
-    });
-    console.log(stats);
-  });
-
-api.getAccountBySteamID('76561198084956266')
-  .then((account) => {
-    console.log(account);
-  });
-  
-  
-
+};
